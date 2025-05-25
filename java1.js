b13a91a02f56d5e49424e3ec12b974dd05cb811b
AbduJalil19 <abdujalilodinaev08@gmail.com>
2025-05-25 14:52:08 +0500

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,119 @@
+// function calculateCircleArea(a)
+// {
+//     a *= a;
+//     a *= 3.14;
+//     return Math.ceil(a);
+// }
+// let a = 5;
+// console.log(calculateCircleArea(a));
+
+
+
+
+// function sum3Digit(a)
+// {
+//     let first = Math.floor(a/100)
+//     let second = Math.floor(a/10) % 10;
+//     let third = a % 10;
+//     return first + second + third;
+// }
+// let a =123;
+// console.log(sum3Digit(a));
+
+
+// function sumOfDigits(a)
+// {
+//     if (a % 2 == 0)
+//     {
+//         a ++ ;
+//     }
+//     else
+//     {
+//         a-=2;
+//     }
+//     return a;
+// }
+// let a = 22;
+// console.log(sumOfDigits(a));
+
+
+
+
+// function doubleEachDigit(a)
+// {
+//     return a*2;
+// }
+// let a = 7132;
+// console.log(doubleEachDigit(a));
+
+
+
+
+// function lessThan100(a,b)
+// {
+//     let sum = a + b;
+//     if (sum > 100)
+//     {
+//         return "false";
+//     }
+//     else
+//     {
+//         return "true";
+//     }
+// }
+// let a = 22;
+// let b = 15;
+// console.log(lessThan100(a,b));
+
+
+
+// function perimetr(a)
+// {
+//     return 4 * a;
+// }
+// let a = 8;
+// console.log(perimetr(a));
+
+
+
+// function area(a)
+// {
+//     return a * a;
+// }
+// let a = 8;
+// console.log(area(a));
+
+
+
+// function averageNum(a,b)
+// {
+//     return (a + b) / 2;
+// }
+// let a = 7;
+// let b = 9;
+// console.log(averageNum(a,b));
+
+
+// function evenOrOdd(a)
+// {
+//     if (a % 2 == 0)
+//     {
+//         return 3;
+//     }
+//     else
+//     {
+//         return 4;
+//     }
+// }
+// let a = 6;
+// console.log(evenOrOdd(a));
+
+
+
+
+// function calculation(a)
+// {
+//     return (a* a * 17) - (6 * a) + 13;
+// }
+// let a = 2;
+// console.log(calculation(a));    
